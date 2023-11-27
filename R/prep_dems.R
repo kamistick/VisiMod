e1009baac8286253e90d1bd3f6478670d9e9c55d
kamistick <katherinemistick@gmail.com>
2023-11-27 14:20:26 -0800

    fix extent mismatch by adding terra::trim() is sab != raster extent.

--- R/prep_dems.R
+++ R/prep_dems.R
@@ -113,6 +113,12 @@ prep_dems <- function(in_dtm, in_dsm, out_dtm, out_dsm){
     terra::as.polygons() |>
     terra::fillHoles()
   
+  # trim if necessary
+  if (ext(sab) != ext(in_dtm)){
+    in_dtm <- terra::trim(in_dtm)
+    in_dsm <- terra::trim(in_dsm)
+  }
+  
   # create output flags
   dtm_fill_flag <- F
   dsm_fill_flag <- F
@@ -139,15 +145,6 @@ prep_dems <- function(in_dtm, in_dsm, out_dtm, out_dsm){
   message(paste0(prttm(), "    dsm has no interior NA values"))
   if (dsm_fill_flag) in_dsm <- terra::crop(in_dsm, sab, mask = T)
   
-  # reset extent & crop to that of the original if they don't match
-  if (dsm_fill_flag & ext_dsm != terra::ext(in_dsm)) {
-    in_dsm <- terra::crop(in_dsm, ext_dsm)
-    terra::ext(in_dsm) <- ext_dsm
-  }
-  if (dtm_fill_flag & ext_dtm != terra::ext(in_dtm)) {
-    in_dtm <- terra::crop(in_dtm, ext_dtm)
-    terra::ext(in_dtm) <- ext_dtm
-  }
   
   # write to file
   if (dtm_fill_flag) terra::writeRaster(in_dtm, out_dtm, overwrite = T)
